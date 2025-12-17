c7538db12fb1df38e743d7842deddf15f1f26cd3
rosie98 <kjr2699@naver.com>
2025-12-17 14:40:31 +0900

    chore: #2 - swagger 문서화

--- src/main/java/com/werp/sero/config/SwaggerConfig.java
+++ src/main/java/com/werp/sero/config/SwaggerConfig.java
@@ -2,6 +2,11 @@ package com.werp.sero.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -9,4 +14,14 @@ import org.springframework.context.annotation.Configuration;
         info = @Info(title = "SERO API", description = "SERO REST API 문서")
 )
 public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("Authorization",
+                                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT"))
+                )
+                .addSecurityItem(new SecurityRequirement().addList("Authorization"))
+                ;
+    }
 }
