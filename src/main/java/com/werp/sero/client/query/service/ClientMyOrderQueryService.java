37ff91ca259439e6fb5971af34451566df92aef1
pilltong22 <rlawlsgh6@naver.com>
2025-12-17 15:35:56 +0900

    fix: #10 주문접수/검토 관련 코드와 병합

---
+++ src/main/java/com/werp/sero/client/query/service/ClientMyOrderQueryService.java
@@ -0,0 +1,8 @@
+package com.werp.sero.client.query.service;
+
+
+import org.springframework.stereotype.Service;
+
+
+public interface ClientMyOrderQueryService {
+}
---
+++ src/main/java/com/werp/sero/client/query/service/ClientMyOrderQueryServiceImpl.java
@@ -0,0 +1,13 @@
+package com.werp.sero.client.query.service;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ClientMyOrderQueryServiceImpl implements ClientMyOrderQueryService {
+
+
+
+}
