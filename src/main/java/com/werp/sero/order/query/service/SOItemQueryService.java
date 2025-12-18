941b41ae6358603ccec1c93f040f650bae7767a3
Yujin <wjd2000db@gmail.com>
2025-12-18 16:41:21 +0900

    refact: #22 - 메소드 하나로 합침으로서 불필요해진 파일 삭제

--- src/main/java/com/werp/sero/order/query/service/SOItemQueryService.java
+++
@@ -1,9 +0,0 @@
-package com.werp.sero.order.query.service;
-
-import com.werp.sero.order.query.dto.SOItemResponseDTO;
-
-import java.util.List;
-
-public interface SOItemQueryService {
-    List<SOItemResponseDTO> findOrderItemsById(final int orderId);
-}
