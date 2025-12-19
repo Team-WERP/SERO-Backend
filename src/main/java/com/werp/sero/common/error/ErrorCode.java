2697fae151d054c4b02c43b304f684e043c57927
HoodRyan <bty0823@naver.com>
2025-12-19 22:32:36 +0900

    feat: #36 - 납품서 미리보기(조회) 기능 개발

--- src/main/java/com/werp/sero/common/error/ErrorCode.java
+++ src/main/java/com/werp/sero/common/error/ErrorCode.java
@@ -57,7 +57,10 @@ public enum ErrorCode {
     SALES_ORDER_ITEM_NOT_FOUND(HttpStatus.NOT_FOUND, "ORDER004", "주문 품목 정보를 찾을 수 없습니다."),
 
     /* GOODS ISSUE */
-    GOODS_ISSUE_NOT_FOUND(HttpStatus.NOT_FOUND, "SHIPPING001", "출고지시 정보를 찾을 수 없습니다.");
+    GOODS_ISSUE_NOT_FOUND(HttpStatus.NOT_FOUND, "SHIPPING001", "출고지시 정보를 찾을 수 없습니다."),
+
+    /* DELIVERY ORDER */
+    DELIVERY_ORDER_NOT_FOUND(HttpStatus.NOT_FOUND, "SHIPPING002", "납품서 정보를 찾을 수 없습니다.");
 
     private final HttpStatus status;
     private final String code;
