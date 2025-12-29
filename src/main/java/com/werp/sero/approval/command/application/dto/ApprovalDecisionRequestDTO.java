b61211785be4d84c6602c8b69f30ea298c28a15d
rosie98 <kjr2699@naver.com>
2025-12-29 16:57:40 +0900

    fix: #129 - 불필요한 import 문 삭제

--- src/main/java/com/werp/sero/approval/command/application/dto/ApprovalDecisionRequestDTO.java
+++ src/main/java/com/werp/sero/approval/command/application/dto/ApprovalDecisionRequestDTO.java
@@ -1,7 +1,6 @@
 package com.werp.sero.approval.command.application.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
