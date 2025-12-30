78d54188bdd2e97fb7a724ae3f91516ce8e8df24
김민지 <ghi512151@gmail.com>
2025-12-31 05:40:24 +0900

    refact: #143 - LineMaterialInfo DTO 분리, daily_capacity 추가 및 cycle_time 제거

--- src/main/java/com/werp/sero/deadline/query/dao/DeadLineMapper.java
+++ src/main/java/com/werp/sero/deadline/query/dao/DeadLineMapper.java
@@ -1,11 +1,6 @@
 package com.werp.sero.deadline.query.dao;
 
-
-import com.werp.sero.deadline.query.dto.DeadLineQueryResponseDTO;
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import com.werp.sero.deadline.query.dto.LineMaterialInfo;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
@@ -16,16 +11,4 @@ import java.util.Optional;
 public interface DeadLineMapper {
     // 자재 코드로 LineMaterial 정보 조회
     Optional<LineMaterialInfo> findLineMaterialByMaterialCode(@Param("materialCode") String materialCode);
-
-    @Getter
-    @NoArgsConstructor
-    @AllArgsConstructor
-    class LineMaterialInfo {
-        private int lineMaterialId;
-        private int productionLineId;
-        private String productionLineName;
-        private String productionLineStatus;
-        private int cycleTime;
-    }
-
 }
---
+++ src/main/java/com/werp/sero/deadline/query/dto/LineMaterialInfo.java
@@ -0,0 +1,16 @@
+package com.werp.sero.deadline.query.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LineMaterialInfo {
+    private int lineMaterialId;
+    private int productionLineId;
+    private String productionLineName;
+    private String productionLineStatus;
+    private int dailyCapacity;
+}
