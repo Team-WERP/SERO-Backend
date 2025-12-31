0771aef569994e3fb0b9a91939ec79d2836cb4b0
pilltong22 <rlawlsgh6@naver.com>
2025-12-31 12:00:45 +0900

    fix: #121 기사 id 수정

--- src/main/java/com/werp/sero/shipping/command/application/service/GoodsIssueCommandServiceImpl.java
+++ src/main/java/com/werp/sero/shipping/command/application/service/GoodsIssueCommandServiceImpl.java
@@ -288,7 +288,7 @@ public class GoodsIssueCommandServiceImpl implements GoodsIssueCommandService {
         String trackingNumber = String.format("SERO-%s-D%03d", today, dailyCount + 1);
 
         // 6-2. 배송기사 조회 (ID=12, 김기사)
-        Employee driver = employeeRepository.findById(12)
+        Employee driver = employeeRepository.findById(23)
                 .orElseThrow(() -> new BusinessException(ErrorCode.EMPLOYEE_NOT_FOUND));
 
         // 6-3. 배송 데이터 생성
