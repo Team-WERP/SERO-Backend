cef8fe4cb3a867e73ce399d83240e0c7e3b81777
김민지 <ghi512151@gmail.com>
2026-01-02 15:38:16 +0900

    Revert "feat: #107 - 생산 대시보드 kpi 조회 api 구현"
    This reverts commit b6eb2280b17423c1f3fb42fb2d2003b9def0932e.

--- src/main/java/com/werp/sero/production/query/service/ProductionDashboardService.java
+++
@@ -1,7 +0,0 @@
-package com.werp.sero.production.query.service;
-
-import com.werp.sero.production.query.dto.ProductionDashboardKpiResponse;
-
-public interface ProductionDashboardService {
-    ProductionDashboardKpiResponse getKpi();
-}
